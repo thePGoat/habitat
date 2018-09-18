3ac7409a491db2b92afe7d18728274ee62221ed6
Scott Hain <shain@chef.io>
2018-09-18 13:12:53 -0700

    Ok revamp some things
    Signed-off-by: Scott Hain <shain@chef.io>

--- .buildkite/scripts/build_windows_release.ps1
+++ .buildkite/scripts/build_windows_release.ps1
@@ -1,13 +1,35 @@
 #!/usr/bin/env powershell
 
 #Requires -Version 5
-Set-PSDebug -Trace 2
+Set-PSDebug -Off #-Trace 1
 
-# Environment variables
+###################
+# Functions
+function BuildPathString([string]$StartingPath, [string]$Path) {
+    if (-not [string]::IsNullOrEmpty($path)) {
+        if (-not [string]::IsNullOrEmpty($StartingPath)) {
+            [string[]]$PathCollection = "$path;$StartingPath" -split ';'
+            $Path = ($PathCollection |
+                Select-Object -Unique |
+                Where-Object {-not [string]::IsNullOrEmpty($_.trim())} |
+                Where-Object {test-path "$_"}
+            ) -join ';'
+        }
+        $path
+    }
+    else {
+        $StartingPath
+    }
+}
+
+
+###################
+# 'main'
 $env:ChocolateyInstall = "$env:ProgramData\Chocolatey"
 $ChocolateyHabitatLibDir = "$env:ChocolateyInstall\lib\habitat_native_dependencies\builds\lib"
 $ChocolateyHabitatIncludeDir = "$env:ChocolateyInstall\lib\habitat_native_dependencies\builds\include"
 $ChocolateyHabitatBinDir = "C:\ProgramData\chocolatey\lib\habitat_native_dependencies\builds\bin"
+$Path="."
 
 # Install Chocolatey
 Write-Host "--- Installing Chocolatey"
@@ -25,12 +47,6 @@ Copy-Item $env:ChocolateyInstall\lib\libzmq_vc120\build\native\bin\libzmq-x64-v1
 choco install libsodium_vc120 --version 1.0.12 --confirm -s https://www.nuget.org/api/v2/
 Copy-Item $env:ChocolateyInstall\lib\libsodium_vc120\build\native\bin\libsodium-x64-v120-mt-1_0_12_0.imp.lib $ChocolateyHabitatLibDir\sodium.lib -Force
 Copy-Item $env:ChocolateyInstall\lib\libsodium_vc120\build\native\bin\libsodium-x64-v120-mt-1_0_12_0.dll $ChocolateyHabitatBinDir\libsodium.dll -Force
-    
-# # We need the Visual C 2013 Runtime for the Win32 ABI Rust
-# choco install 'vcredist2013' --confirm --allowemptychecksum
-
-# # We need the Visual C++ tools to build Rust crates (provides a compiler and linker)
-# choco install 'visualcppbuildtools' --version '14.0.25123' --confirm --allowemptychecksum
 
 # 7zip!
 choco install 7zip --version '16.02.0.20160811' --confirm
@@ -38,16 +54,19 @@ choco install 7zip --version '16.02.0.20160811' --confirm
 # Install some rust
 Write-Host "--- Installing rustup and stable-x86_64-pc-windows-msvc Rust."
 invoke-restmethod -usebasicparsing 'https://static.rust-lang.org/rustup/dist/i686-pc-windows-gnu/rustup-init.exe' -outfile 'rustup-init.exe'
-./rustup-init.exe -y --default-toolchain stable-x86_64-pc-windows-msvc --no-modify-path
+./rustup-init.exe -y --no-modify-path
+$env:PATH                   = BuildPathString -StartingPath $env:PATH    -Path "$env:USERPROFILE\.cargo\bin"
+rustup install stable-x86_64-pc-windows-msvc
 
 # Install protobuf helper stuff
 choco install protoc -y
 invoke-expression "cargo install protobuf"
 
-$env:PATH                   = New-PathString -StartingPath $env:PATH    -Path 'C:\Program Files\7-Zip'
-$env:PATH                   = New-PathString -StartingPath $env:PATH    -Path $ChocolateyHabitatBinDir
-$env:LIB                    = New-PathString -StartingPath $env:LIB     -Path $ChocolateyHabitatLibDir
-$env:INCLUDE                = New-PathString -StartingPath $env:INCLUDE -Path $ChocolateyHabitatIncludeDir
+$env:PATH                   = BuildPathString -StartingPath $env:PATH    -Path 'C:\Program Files\7-Zip'
+$env:PATH                   = BuildPathString -StartingPath $env:PATH    -Path $ChocolateyHabitatBinDir
+#$env:PATH                   = New-PathString -StartingPath $env:PATH    -Path Env:
+$env:LIB                    = BuildPathString -StartingPath $env:LIB     -Path $ChocolateyHabitatLibDir
+$env:INCLUDE                = BuildPathString -StartingPath $env:INCLUDE -Path $ChocolateyHabitatIncludeDir
 $env:SODIUM_LIB_DIR         = $ChocolateyHabitatLibDir
 $env:LIBARCHIVE_INCLUDE_DIR = $ChocolateyHabitatIncludeDir
 $env:LIBARCHIVE_LIB_DIR     = $ChocolateyHabitatLibDir
@@ -78,27 +97,3 @@ Invoke-Expression "$cargo build" -ErrorAction Stop
 Pop-Location
 
 exit $LASTEXITCODE
-
-# original string:
-# Invoke-Build $Path -Clean:$Clean -Release:$Release -Check:$Check -Protocols:$protocols
-# Invoke-Build "." -Clean:True -Release:$Release -Check:$Check -Protocols:$protocols
-
-
-# ### FUNCTIONS ###
-# function Invoke-Build([string]$Path, [switch]$Clean, [switch]$Release, [switch]$Check, [switch]$Protocols) {
-#   $Path = Resolve-Path $Path
-
-#   $cargo = "cargo"
-
-#   Push-Location "$Path"
-#   if($Clean) {
-#       invoke-expression "$cargo clean"
-#   }
-#   if($Check) {
-#       Invoke-Expression "$cargo check" -ErrorAction Stop
-#   }
-#   else {
-#       Invoke-Expression "$cargo build $(if ($Release) { '--release' }) $(if ($Protocols) { '--features protocols' })" -ErrorAction Stop
-#   }
-#   Pop-Location
-# }

d99f6c5fdc7659e8c330295163f440d4c4e6cb4b
Scott Hain <shain@chef.io>
2018-09-18 10:39:48 -0700

    Test initial installs and setup

---
+++ .buildkite/scripts/build_windows_release.ps1
@@ -0,0 +1,47 @@
+#!/usr/bin/env powershell
+
+#Requires -Version 5
+
+# Environment variables
+$env:ChocolateyInstall = "$env:ProgramData\Chocolatey"
+$ChocolateyHabitatLibDir = "$env:ChocolateyInstall\lib\habitat_native_dependencies\builds\lib"
+$ChocolateyHabitatIncludeDir = "$env:ChocolateyInstall\lib\habitat_native_dependencies\builds\include"
+$ChocolateyHabitatBinDir = "C:\ProgramData\chocolatey\lib\habitat_native_dependencies\builds\bin"
+
+
+# Install Chocolatey
+Write-Host "Installing Chocolatey"
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) | out-null
+
+# Install hab native dependencies
+choco install habitat_native_dependencies --confirm -s https://www.myget.org/F/habitat/api/v2  --allowemptychecksums
+
+# Install libzmq
+choco install libzmq_vc120 --version 4.2.3 --confirm -s https://www.nuget.org/api/v2/ --allowemptychecksums
+Copy-Item $env:ChocolateyInstall\lib\libzmq_vc120\build\native\bin\libzmq-x64-v120-mt-4_2_3_0.imp.lib $ChocolateyHabitatLibDir\zmq.lib -Force
+Copy-Item $env:ChocolateyInstall\lib\libzmq_vc120\build\native\bin\libzmq-x64-v120-mt-4_2_3_0.dll $ChocolateyHabitatBinDir\libzmq.dll -Force
+
+# Install libsodium
+choco install libsodium_vc120 --version 1.0.12 --confirm -s https://www.nuget.org/api/v2/
+Copy-Item $env:ChocolateyInstall\lib\libsodium_vc120\build\native\bin\libsodium-x64-v120-mt-1_0_12_0.imp.lib $ChocolateyHabitatLibDir\sodium.lib -Force
+Copy-Item $env:ChocolateyInstall\lib\libsodium_vc120\build\native\bin\libsodium-x64-v120-mt-1_0_12_0.dll $ChocolateyHabitatBinDir\libsodium.dll -Force
+    
+# We need the Visual C 2013 Runtime for the Win32 ABI Rust
+choco install 'vcredist2013' --confirm --allowemptychecksum
+
+# We need the Visual C++ tools to build Rust crates (provides a compiler and linker)
+choco install 'visualcppbuildtools' --version '14.0.25123' --confirm --allowemptychecksum
+
+# 7zip!
+choco install 7zip --version '16.02.0.20160811' --confirm
+
+# Install some rust
+Write-Host "Installing rustup and stable-x86_64-pc-windows-msvc Rust."
+invoke-restmethod -usebasicparsing 'https://static.rust-lang.org/rustup/dist/i686-pc-windows-gnu/rustup-init.exe' -outfile 'rustup-init.exe'
+./rustup-init.exe -y --default-toolchain stable-x86_64-pc-windows-msvc --no-modify-path
+
+# Install protobuf helper stuff
+choco install protoc -y
+$cargo = Get-CargoCommand
+invoke-expression "$cargo install protobuf"
+

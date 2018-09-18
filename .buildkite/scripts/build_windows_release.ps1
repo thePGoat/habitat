f1b5a4560cdfab460528cb5c36b475a4c3ba047a
Scott Hain <shain@chef.io>
2018-09-18 11:40:26 -0700

    Add build section to see what happens
    Signed-off-by: Scott Hain <shain@chef.io>

--- .buildkite/scripts/build_windows_release.ps1
+++ .buildkite/scripts/build_windows_release.ps1
@@ -8,7 +8,6 @@ $ChocolateyHabitatLibDir = "$env:ChocolateyInstall\lib\habitat_native_dependenci
 $ChocolateyHabitatIncludeDir = "$env:ChocolateyInstall\lib\habitat_native_dependencies\builds\include"
 $ChocolateyHabitatBinDir = "C:\ProgramData\chocolatey\lib\habitat_native_dependencies\builds\bin"
 
-
 # Install Chocolatey
 Write-Host "Installing Chocolatey"
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) | out-null
@@ -26,11 +25,11 @@ choco install libsodium_vc120 --version 1.0.12 --confirm -s https://www.nuget.or
 Copy-Item $env:ChocolateyInstall\lib\libsodium_vc120\build\native\bin\libsodium-x64-v120-mt-1_0_12_0.imp.lib $ChocolateyHabitatLibDir\sodium.lib -Force
 Copy-Item $env:ChocolateyInstall\lib\libsodium_vc120\build\native\bin\libsodium-x64-v120-mt-1_0_12_0.dll $ChocolateyHabitatBinDir\libsodium.dll -Force
     
-# We need the Visual C 2013 Runtime for the Win32 ABI Rust
-choco install 'vcredist2013' --confirm --allowemptychecksum
+# # We need the Visual C 2013 Runtime for the Win32 ABI Rust
+# choco install 'vcredist2013' --confirm --allowemptychecksum
 
-# We need the Visual C++ tools to build Rust crates (provides a compiler and linker)
-choco install 'visualcppbuildtools' --version '14.0.25123' --confirm --allowemptychecksum
+# # We need the Visual C++ tools to build Rust crates (provides a compiler and linker)
+# choco install 'visualcppbuildtools' --version '14.0.25123' --confirm --allowemptychecksum
 
 # 7zip!
 choco install 7zip --version '16.02.0.20160811' --confirm
@@ -44,3 +43,60 @@ invoke-restmethod -usebasicparsing 'https://static.rust-lang.org/rustup/dist/i68
 choco install protoc -y
 invoke-expression "cargo install protobuf"
 
+$env:PATH                   = New-PathString -StartingPath $env:PATH    -Path 'C:\Program Files\7-Zip'
+$env:PATH                   = New-PathString -StartingPath $env:PATH    -Path $ChocolateyHabitatBinDir
+$env:LIB                    = New-PathString -StartingPath $env:LIB     -Path $ChocolateyHabitatLibDir
+$env:INCLUDE                = New-PathString -StartingPath $env:INCLUDE -Path $ChocolateyHabitatIncludeDir
+$env:SODIUM_LIB_DIR         = $ChocolateyHabitatLibDir
+$env:LIBARCHIVE_INCLUDE_DIR = $ChocolateyHabitatIncludeDir
+$env:LIBARCHIVE_LIB_DIR     = $ChocolateyHabitatLibDir
+$env:OPENSSL_LIBS           = 'ssleay32:libeay32'
+$env:OPENSSL_LIB_DIR        = $ChocolateyHabitatLibDir
+$env:OPENSSL_INCLUDE_DIR    = $ChocolateyHabitatIncludeDir
+$env:LIBZMQ_PREFIX          = Split-Path $ChocolateyHabitatLibDir -Parent
+
+Write-Host "Downloading cacerts.pem"
+$current_protocols = [Net.ServicePointManager]::SecurityProtocol
+try {
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    Invoke-WebRequest -UseBasicParsing -Uri "http://curl.haxx.se/ca/cacert.pem" -OutFile "$env:TEMP\cacert.pem"
+}
+finally {
+    [Net.ServicePointManager]::SecurityProtocol = $current_protocols
+}
+
+$env:SSL_CERT_FILE="$env:TEMP\cacert.pem"
+$env:PROTOBUF_PREFIX=$env:ChocolateyInstall
+
+# test build
+$Path = Resolve-Path $Path
+$cargo = "cargo"
+Push-Location "$Path"
+Invoke-Expression "$cargo build" -ErrorAction Stop
+Pop-Location
+
+exit $LASTEXITCODE
+
+# original string:
+# Invoke-Build $Path -Clean:$Clean -Release:$Release -Check:$Check -Protocols:$protocols
+# Invoke-Build "." -Clean:True -Release:$Release -Check:$Check -Protocols:$protocols
+
+
+# ### FUNCTIONS ###
+# function Invoke-Build([string]$Path, [switch]$Clean, [switch]$Release, [switch]$Check, [switch]$Protocols) {
+#   $Path = Resolve-Path $Path
+
+#   $cargo = "cargo"
+
+#   Push-Location "$Path"
+#   if($Clean) {
+#       invoke-expression "$cargo clean"
+#   }
+#   if($Check) {
+#       Invoke-Expression "$cargo check" -ErrorAction Stop
+#   }
+#   else {
+#       Invoke-Expression "$cargo build $(if ($Release) { '--release' }) $(if ($Protocols) { '--features protocols' })" -ErrorAction Stop
+#   }
+#   Pop-Location
+# }
